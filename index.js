37abd642fca9fce2c9cb9b94759701768df7a901
Christine Emrich <christine.emrich@web.de>
2017-03-21 19:34:17 +0100

    Add version to api url

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const shows = require('./routes/shows');
 
 const app = express();
 
-app.get('/shows', shows.findAll);
-app.get('/shows/:channelId', shows.findByChannel);
+app.get('/v1/shows', shows.findAll);
+app.get('/v1/shows/:channelId', shows.findByChannel);
 
 app.listen(4000);
 console.log('Listening on port 3000...');
