b976d3b8567380b3c580cd590f23f5bbe85f26a1
Christine Emrich <christine.emrich@web.de>
2020-08-26 13:24:50 +0200

    Add basic downloader tests

---
+++ test/downloader.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const channelInfoList = require('../models/ChannelInfoList.js');
+const showDownloader = require('../downloader/show-downloader');
+
+context('Show downloader', function () {
+
+	describe('#getShow()', function () {
+
+		function testChannel(channelId) {
+			it(`should not crash when running for ${channelId}`, async function () {
+				await showDownloader.getShow(channelId)
+			});
+		}
+
+		const channelIds = Object.keys(channelInfoList);
+		channelIds.forEach(testChannel);
+
+	});
+
+});
