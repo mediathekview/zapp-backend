5d423deefdf3cf67b766ff2d5a798e25afed6ff3
Christine Emrich <christine.emrich@web.de>
2020-08-26 11:20:51 +0200

    Use new ard api #8

--- downloader/ard-downloader.js
+++ downloader/ard-downloader.js
@@ -1,140 +1,72 @@
 const request = require('request');
 const moment = require('moment-timezone');
-const xml2js = require('xml2js');
 const Show = require('../models/Show');
 
-const url = 'http://programm.ard.de/TV/Programm/Load/NavJetztImTV35';
-const parser = new xml2js.Parser({ strict: false });
+const url = 'https://programm.ard.de/TV/Export/Now';
 
 const channelIdMap = {
-	'Das Erste': ['das_erste'],
-	'ONE': ['one'],
-	'tagesschau24': ['tagesschau24'],
-	'ARD-alpha': ['ard_alpha'],
-	'BR Fernsehen': ['br_nord', 'br_sued'],
-	'hr-fernsehen': ['hr'],
-	'SR Fernsehen': ['sr'],
-	'WDR Fernsehen': ['wdr'],
-	'NDR Fernsehen': ['ndr_sh', 'ndr_nds', 'ndr_mv', 'ndr_hh'],
-	'MDR Fernsehen': ['mdr_thueringen', 'mdr_sachsen_anhalt', 'mdr_sachsen'],
-	'SWR Ferns. BW': ['swr_bw'],
-	'SWR Ferns. RP': ['swr_rp'],
-	'rbb Fernsehen': ['rbb_berlin', 'rbb_brandenburg'],
-	'Radio Bremen': ['rb'],
+	"das_erste": "daserste",
+	"tagesschau24": "tagesschau24",
+	"br_sued": "br-süd",
+	"br_nord": "br-süd",
+	"hr": "hr",
+	"mdr_thueringen": "mdr-sachsen",
+	"mdr_sachsen_anhalt": "mdr-sachsen",
+	"mdr_sachsen": "mdr-sachsen",
+	"ndr_sh": "ndr-niedersachsen",
+	"ndr_nds": "ndr-niedersachsen",
+	"ndr_mv": "ndr-niedersachsen",
+	"ndr_hh": "ndr-niedersachsen",
+	"rb": "radiobremen",
+	"rbb_berlin": "rbb-brandenburg",
+	"rbb_brandenburg": "rbb-brandenburg",
+	"sr": "sr",
+	"swr_bw": "swr-württemberg",
+	"swr_rp": "swr-württemberg",
+	"ard_alpha": "alpha",
 };
 
-exports.channelIds = [
-	'das_erste',
-	'one',
-	'tagesschau24',
-	'ard_alpha',
-	'br_nord',
-	'br_sued',
-	'hr',
-	'sr',
-	'wdr',
-	'ndr_sh',
-	'ndr_nds',
-	'ndr_mv',
-	'ndr_hh',
-	'mdr_thueringen',
-	'mdr_sachsen_anhalt',
-	'mdr_sachsen',
-	'swr_bw',
-	'swr_rp',
-	'rbb_berlin',
-	'rbb_brandenburg',
-	'rb',
-];
+exports.channelIds = Object.keys(channelIdMap);
 
-let runningPromise = null;
+function getShow(json, channelId, mediathekChannelName) {
+	let broadcasts = json['events'];
 
-function parseShows(showData, showObj) {
-	let infoLine = showData.IMG[0].SPAN[1]._.trim();
-	let channels = infoLine.split('|').map((s) => s.trim());
-	let times = channels.pop().split(/ - | /).map((s) => s.trim());
-
-	let show = new Show(showData.IMG[0].B[0]);
-	show.infoUrl = 'http://programm.ard.de' + showData.$.HREF;
-	show.startTime = moment.tz(times[0], 'HH:mm', 'Europe/Berlin');
-	show.endTime = moment.tz(times[1], 'HH:mm', 'Europe/Berlin');
-	show.fixMidnightTime();
-
-	for (let channel of channels) {
-		let channelIds = channelIdMap[channel] || [];
-		for (let channelId of channelIds) {
-			if (show.isRunningNow()) {
-				show.channel = channelId;
-				showObj[channelId] = show.clone();
-			} else {
-				showObj[channelId] = null;
-			}
-		}
+	if (!broadcasts) {
+		return null;
 	}
-}
-
-function parse(xml) {
-	return new Promise((resolve, reject) => {
-		parser.parseString(xml, (err, data) => {
-			if (err) {
-				reject('parsing error ' + err);
-			} else {
-				resolve(data);
-			}
-		});
-	});
-}
-
-function getAllShows(xml) {
-	let validXml = `<root>${xml}</root>`;
-	return parse(validXml)
-		.then(data => {
-			let showObj = {};
-			for (let item of data.ROOT.LI) {
-				parseShows(item.A[0], showObj);
-			}
-			return Promise.resolve(showObj);
-		});
-}
-
-function requestShow() {
-	return new Promise((resolve, reject) => {
-		request.get({ url: url }, (err, res, data) => {
-				if (err) {
-					reject(err);
-				} else if (res.statusCode !== 200) {
-					return reject('wrong status code: ' + res.statusCode);
-				} else {
-					return resolve(data);
-				}
-		});
-	});
+	
+	for (let entryKey in broadcasts) {
+		const entry = broadcasts[entryKey].now;
+		
+		if (entry && entry.channel.mediathek_name === mediathekChannelName) {
+			let show = new Show(entry.title);
+			show.subtitle = entry.sub_title;
+			show.description = entry.short_text;
+			show.channel = channelId;
+			show.startTime = moment(entry.start);
+			show.endTime = moment(entry.stop);
+			return show;
+		}			
+	}
+	
+	return null;
 }
 
 exports.getShow = function (channelId) {
-	function resolveFromShows(shows) {
-		let show = shows[channelId];
-		if (!show) {
-			throw 'show info currently not available';
-		} else {
-			return show;
-		}
-	}
-
-	if (runningPromise !== null) {
-		return runningPromise.then(resolveFromShows);
-	}
-
-	runningPromise = requestShow().then(getAllShows);
-
-	return runningPromise
-		.then(() => {
-			let p = runningPromise;
-			runningPromise = null;
-			return p;
-		}, err => {
-			runningPromise = null;
-			throw err;
-		})
-		.then(resolveFromShows);
+	return new Promise((resolve, reject) => {
+		request.get({ url: url, json: true }, (err, res, data) => {
+			if (err) {
+				reject(err);
+			} else if (res.statusCode !== 200) {
+				return reject('wrong status code for getShow: ' + res.statusCode);
+			} else {
+				let show = getShow(data, channelId, channelIdMap[channelId]);
+				if (show === null) {
+					reject('show info currently not available');
+				} else {
+					return resolve(show);
+				}
+			}
+		});
+	});
 };
