da9f542e229a7865898100610f33b48992aadb5f
Christine Coenen <code.coenen@gmail.com>
2024-07-09 16:10:26 +0200

    Send CORS headers

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use((_, res, next) => {
 	res.setHeader("Version", version);
+
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
 	next();
 });
 
